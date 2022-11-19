938042748cce34ff7370131a6c0fb4dd2f3546ee
Vinit Gupta <vinitman35@gmail.com>
2022-11-19 15:39:10 +0530

    add sign in with github

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,11 @@
+import {useSession, signOut}  from "next-auth/react"
 import Link from "next/link"
 
 const Header = () => {
+    const { data: session, status } = useSession();
+
+    
+
     return (
     <nav className={`flex justify-center gap-10 p-5 text-white mx-auto z-10`}>
         <div className="max-w-7xl flex-1 flex justify-between gap-10">
@@ -11,10 +16,22 @@ const Header = () => {
             <div className="flex items-center cursor-pointer space-x-10 font-head font-bold text-[1.2rem]">
                 <div>Search</div>
                 <div>Github</div>
-                <div>Developer</div>
-                <Link href="/auth">
-                <div className="bg-[#8432ff] py-2 px-8 text-white rounded-full font-bold hover:bg-[#fd29ef]">Sign In</div>
-                </Link>
+                {session ? 
+                    <div className={`bg-[#8432ff] py-2 px-8 text-white rounded-full font-bold hover:bg-[#fd29ef] ${!session || status!=='authenticated' ? "loading" : "loaded"}`}>
+                        Add Post
+                    </div>
+                    :
+                    <div className={`${!session && status==='loading' ? "loading" : "loaded"}`}>Developer</div>
+                }
+
+                {
+                session ?
+                    <div onClick={()=>{signOut()}} className={`${!session || status!=='authenticated' ? "loading" : "loaded"}`}>Sign Out</div>
+                    :
+                    <Link href="/auth">
+                        <div className={`bg-[#8432ff] py-2 px-8 text-white rounded-full font-bold hover:bg-[#fd29ef] ${!session && status==='loading' ? "loading" : "loaded"}`}>Sign In</div>
+                    </Link>
+                }
 
             </div>
 
--- components/SignIn.js
+++ components/SignIn.js
@@ -1,16 +1,32 @@
+import {useSession, signIn, signOut}  from "next-auth/react"
+import {useRouter} from "next/router"
 import Image from "next/image";
-import { useState } from "react"
-import Github from "../public/github.png"
+import { useState,useEffect } from "react"
+import Github from "../public/github.png";
 
 const SignIn = () => {
+    const { data: session, status } = useSession();
     const [activeTab, setActiveTab] = useState("login");
     const toggleActiveTab = (e) => {
         if(e.target.innerHTML==="Sign Up") setActiveTab("signup");
         else setActiveTab("login");
     }
+
+    const handleSignup = () => {
+        const data = signIn();
+        
+    }
+
+    let router= useRouter()
+    // condition base redirecting
+    const redirect = () => {   
+        router.push('/')
+    }
+    
+
     return (
         <section className="w-[100%] h-[80%] flex items-center justify-center">
-            <div className="w-[70%] sm:w-[35%] h-[80%] white-glassmorphism flex flex-col min-w-[320px] sm:min-w-[426px]">
+            {!session ? <div className="w-[70%] sm:w-[35%] h-[80%] white-glassmorphism flex flex-col min-w-[320px] sm:min-w-[426px]">
                 <div className="w-full h-16 font-head font-bold text-white text-3xl flex justify-evenly items-center text-shadow px-6 relative">
                 <div className={`w-[35%] transition duration-1500 ease-in-out white-glassmorphism absolute bottom-0 ${activeTab==="login" ? "left-10" : "right-10"}`}></div>
                     <div className={`flex-1 flex items-center justify-center h-[100%] hover:cursor-pointer`} onClick={(e) => toggleActiveTab(e)}>Sign In</div>
@@ -24,7 +40,7 @@ const SignIn = () => {
                             Welcome back, let's start contributing. <br/>
                             After you Sign In 👇
                         </p>
-                        <div className="w-[60%] flex justify-center items-center gap-5 text-2xl object-contain bg-[#757EC7] p-4 rounded-full hover:cursor-pointer"> <Image src={Github.src} height={22} width={30}/> Sign In with Github</div>
+                        <div className="w-[60%] flex justify-center items-center gap-5 text-2xl object-contain bg-[#757EC7] p-4 rounded-full hover:cursor-pointer" onClick={() => signIn()}> <Image src={Github.src} height={22} width={30}/> Sign In with Github</div>
                     </div>
                     :
                     <div className="text-white w-full h-full flex flex-col items-center justify-evenly" id="login">
@@ -36,6 +52,14 @@ const SignIn = () => {
                     </div>}
                 </div>
             </div>
+            :
+            <div className="w-[70%] sm:w-[35%] h-[80%] white-glassmorphism text-white flex flex-col justify-center items-center min-w-[320px] sm:min-w-[426px]">
+                <div className="w-[70%] flex-[0.4] text-2xl text-center">{`Hello ${session.user.name.split(" ")[0]}, you're already logged in. Create a post or 👇👇`}</div>
+                <div className="w-[60%] flex justify-center items-center gap-5 text-2xl object-contain bg-[#9b9dab] text-[#2a2a2a] p-4 rounded-full hover:cursor-pointer" onClick={() => {
+                    redirect();
+                }} >{`Redirect to home page`}</div>
+            </div>
+            }
         </section>
     )
 }
